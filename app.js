9fafc03f51871703e4a66555de00a8a9dd6cc40a
dogukan <Karademird67@gmail.com>
2022-09-18 23:24:55 +0200

    new book function

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 let myLibrary = []
 
 function Book() {
-    this.tile = title
-    this.author = author
-    this.genre = genre
-    this.pages = pages
-    this.readStatus = readStatus
-    this.coverImage = coverImage
+    this.tile = undefined
+    this.author = undefined
+    this.pages = undefined
+    this.readStatus = undefined
+    this.coverImage = undefined
 }
 
 
+function addBookToLibrary(title, author, genre, pages, readStatus, coverImage ) {
+    const newBook = new Book()
+    args = [...arguments]
+
+    newBook.tile = args[0]
+    newBook.author = args[1]
+    newBook.genre = args[2]
+    newBook.pages = args[3]
+    newBook.readStatus = args[4]
+    newBook.coverImage = args[5]
+    
+    myLibrary.push( newBook )
+}
