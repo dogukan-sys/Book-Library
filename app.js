94e5c188cdd1162e74e3b5297126255e49f65c95
dogukan <Karademird67@gmail.com>
2022-09-18 22:37:06 +0200

    book constructor

---
+++ app.js
@@ -0,0 +1,12 @@
+let myLibrary = []
+
+function Book() {
+    this.tile = title
+    this.author = author
+    this.genre = genre
+    this.pages = pages
+    this.readStatus = readStatus
+    this.coverImage = coverImage
+}
+
+
