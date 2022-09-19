55719e524d1b10f9793acd08995b1b499c6ae8ac
dogukan <Karademird67@gmail.com>
2022-09-19 14:58:51 +0200

    Card creation functino

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-let myLibrary = []
+let myLibrary = [
+
+]
 
 function Book() {
-    this.tile = undefined
+    this.title = undefined
     this.author = undefined
+    this.genre = undefined
     this.pages = undefined
     this.readStatus = undefined
 }
 
 
-function addBookToLibrary(title, author, genre, pages, readStatus, coverImage ) {
+function addBookToLibrary() {
     const newBook = new Book()
     args = [...arguments]
 
-    newBook.tile = args[0]
+    newBook.title = args[0]
     newBook.author = args[1]
     newBook.genre = args[2]
     newBook.pages = args[3]
@@ -20,3 +23,55 @@ function addBookToLibrary(title, author, genre, pages, readStatus, coverImage ) 
     
     myLibrary.push( newBook )
 }
+
+
+function createCard(title, author, genre, pages, readStatus) {
+    // Getting the elements
+    const card_container = document.querySelector('.card_container')
+    const card = document.createElement('div')
+    const card_title = document.createElement('p')
+    const card_author = document.createElement('p')
+    const card_genre = document.createElement('p')
+    const card_pages = document.createElement('p')
+    const card_status = document.createElement('p')
+
+    // Setting Class names for css
+
+    card.className = 'card'
+    card_title.className = 'card_title'
+    card_author.className = 'card_author'
+    card_genre.className = 'card_genre'
+    card_pages.className = 'card_pages'
+    card_status.className = 'card_status' 
+
+    // Setting corret innerText for each element
+
+    card_title.innerText = title
+    card_author.innerText = author
+    card_genre.innerText = genre
+    card_pages.innerText = pages
+    card_status.innerText = readStatus
+
+
+    // Appending everything correctly
+    card.appendChild(card_title)
+    card.appendChild(card_author)
+    card.appendChild(card_genre)
+    card.appendChild(card_pages)
+    card.appendChild(card_status)
+    card_container.appendChild(card)
+}
+
+function libToCard() {
+    for (i=0; i< myLibrary.length; i++) {
+        console.log(myLibrary[i].title)
+    }
+}
+
+addBookToLibrary('One Piece', 'Eichiro Oda', 'Shonen', '100 volumes', 'reading')
+
+addBookToLibrary('One Piece', 'Eichiro Oda', 'Shonen', '100 volumes', 'reading')
+
+addBookToLibrary('One Piece', 'Eichiro Oda', 'Shonen', '100 volumes', 'reading')
+
+libToCard()
